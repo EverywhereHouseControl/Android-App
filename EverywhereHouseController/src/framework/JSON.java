005e86f6d010e3175baff12db091eead312435eb
Colin Tirado <ctiradocaa@gmail.com>
2014-03-08 15:40:51 +0100

    [MODIFIED] Implemented Singlenton's pattern

--- EverywhereHouseController/src/framework/JSON.java
+++ EverywhereHouseController/src/framework/JSON.java
@@ -9,19 +9,28 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.content.Context;
-import android.os.Bundle;
 import android.util.Log;
 
 
 public class JSON 
 {
-	String file = null;
+	private static JSON instance;
+	public ArrayList<String> rooms;
+	public ArrayList<String> items;
+		
 	private static final String TAG_NAME = "name";
 	private static final String TAG_USER = "User";
 	private static final String TAG_ROOMS = "Rooms";
 	private static final String TAG_ITEMS = "items";
+	private String file = null;
 	
+	private JSON(){}
 	
+	public static synchronized JSON getInstance(Context c){
+		if (instance==null)
+			instance=new JSON(c);
+		return instance;			
+	}
 	
 
 	public JSON(Context c) 
@@ -33,7 +42,12 @@ public class JSON
             byte[] buffer = new byte[size];
             is.read(buffer);
             is.close();
-            file = new String(buffer, "UTF-8");
+            this.file = new String(buffer, "UTF-8");
+            try {
+				loadJSON();
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
 
         } 
         catch (IOException ex) 
@@ -42,7 +56,7 @@ public class JSON
         }
     }
 
-	public void loadJSON() throws JSONException 
+	private void loadJSON() throws JSONException 
 	{
 		JSONObject obj = new JSONObject(file);
 		try 
@@ -72,7 +86,7 @@ public class JSON
 	
 	public ArrayList<String> getRooms() throws JSONException 
 	{
-		JSONObject obj = new JSONObject(file);
+		JSONObject obj = new JSONObject(this.file);
 		ArrayList<String> rooms = new ArrayList<String>();
 
 		try 
@@ -91,16 +105,16 @@ public class JSON
 		return rooms;
 	}
 	
-	public ArrayList<String> getItems() throws JSONException 
+	public ArrayList<String> getAllItems() throws JSONException 
 	{
 		
-		JSONObject obj = new JSONObject(file);
+		JSONObject obj = new JSONObject(this.file);
 		ArrayList<String> rooms = new ArrayList<String>();
 		
 		try 
 		{
 			JSONObject habitaciones = obj.getJSONObject(TAG_ROOMS);
-			for (int i = 0; i < habitaciones.length(); i++)
+			for (int i = 1; i < habitaciones.length(); i++)
 			{
 				JSONObject habitacion = habitaciones.getJSONObject("R"+i);
 				
@@ -121,6 +135,41 @@ public class JSON
 			{
 				e.printStackTrace();
 			}
+		return rooms;
+	}
+	
+	public ArrayList<String> getItems(String roomName) throws JSONException 
+	{
+		
+		JSONObject obj = new JSONObject(this.file);
+		ArrayList<String> rooms = new ArrayList<String>();
+		
+		try 
+		{
+			JSONObject habitaciones = obj.getJSONObject(TAG_ROOMS);
+			for (int i = 1; i < habitaciones.length(); i++)
+			{
+				JSONObject habitacion = habitaciones.getJSONObject("R"+i);
+				
+				System.out.println(habitacion.getString(TAG_NAME));
+				
+				Log.e("COLIN_TAG", habitacion.getString(TAG_NAME));
+				JSONArray items = habitacion.getJSONArray(TAG_ITEMS);
+				if (roomName.equals(habitacion.getString(TAG_NAME))){
+					for (int j = 0; j < items.length(); j++)
+						{
+							rooms.add(items.getString(j));
+							Log.e("COLIN_TAG",items.getString(j));
+							System.out.println(items.getString(j));
+						}
+				}
+			}
+			} 
+		catch (JSONException e) 
+		{
+			e.printStackTrace();
+		}
+		
 		return rooms;
 	}
 }
