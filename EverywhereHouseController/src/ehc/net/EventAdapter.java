b36777b582ea96340d8dc9f4f58bca8695694577
Colin Tirado <Sebastian@Sebastian-PC>
2014-04-18 22:47:42 +0200

    [ADDED] Event info into event list

--- EverywhereHouseController/src/ehc/net/EventAdapter.java
+++ EverywhereHouseController/src/ehc/net/EventAdapter.java
@@ -39,8 +39,15 @@ public class EventAdapter extends BaseAdapter {
 			LayoutInflater _infalInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 			convertView = _infalInflater.inflate(R.layout.event_fragment, null); 
 		}
-		TextView item = (TextView) convertView.findViewById(R.id.fragment_name);
-		item.setText(event.get(position).getName());
+		TextView name = (TextView) convertView.findViewById(R.id.fragment_name);
+		TextView item = (TextView) convertView.findViewById(R.id.fragment_item);
+		TextView creator = (TextView) convertView.findViewById(R.id.fragment_creator);
+		TextView hour = (TextView) convertView.findViewById(R.id.fragment_hour);
+		
+		name.setText(event.get(position).getName());
+		item.setText(Integer.toString(event.get(position).getItem()));
+		creator.setText(event.get(position).getCreator());
+		hour.setText(event.get(position).getHour() + ":" + event.get(position).getMinute());
 		
 		return convertView;
 	}
